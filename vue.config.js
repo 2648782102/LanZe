0d24838b3976dbacc81f08e8eadf919b2278d15f
15855808379 <11055956+h2648782102@user.noreply.gitee.com>
2022-06-03 23:20:16 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -11,7 +11,7 @@ module.exports = {
     // 指定子路径。比如将你的应用部署在
     // https://www.foobar.com/my-app/
     // 那么将这个值改为 '/my-app/'
-    baseUrl: "./", // 构建好的文件输出到哪里
+    baseUrl: "/", // 构建好的文件输出到哪里
   
     outputDir: "dist", // where to put static assets (js/css/img/font/...) // 是否在保存时使用‘eslint-loader’进行检查 // 有效值: true | false | 'error' // 当设置为‘error’时，检查出的错误会触发编译失败
     lintOnSave: true, // 使用带有浏览器内编译器的完整构建版本 // https://vuejs.org/v2/guide/installation.html#Runtime-Compiler-vs-Runtime-only
